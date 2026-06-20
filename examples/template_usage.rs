21f08a923ea1924792f75dd2a022518e645f7377
Forge <forge@phenotype.local>
2026-06-20 05:20:05 -0700

    chore(absorb): land 13 missing template files from pheno-cargo-template

--- examples/template_usage.rs
+++ examples/template_usage.rs
@@ -1,4 +1,4 @@
-//! Quickstart example for HexaKit.
+//! Quickstart example for pheno-cargo-template.
 //!
 //! Run with:
 //!   cargo run --example template_usage
@@ -7,9 +7,9 @@ use pheno_cargo_template::crate_name;
 
 fn main() {
     println!("Template crate name: {}", crate_name());
-    println!("This binary was generated from the HexaKit template.");
+    println!("This binary was generated from the pheno-cargo-template template.");
     println!();
     println!("To generate your own crate from this template:");
     println!("  cargo install cargo-generate");
-    println!("  cargo generate --git https://github.com/KooshaPari/HexaKit");
+    println!("  cargo generate --git https://github.com/KooshaPari/pheno-cargo-template");
 }
